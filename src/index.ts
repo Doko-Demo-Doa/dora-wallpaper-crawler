b1974dc910c4a7ce14e763e9465b45558d1d62b3
Quan Pham <doraemonfanclub@gmail.com>
2025-06-30 00:17:53 +0700

    add /all for health check

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,48 @@
-import { Elysia } from "elysia";
-import { swagger } from "@elysiajs/swagger";
 import { cron, Patterns } from "@elysiajs/cron";
+import { swagger } from "@elysiajs/swagger";
 import { inArray } from "drizzle-orm";
+import { Elysia } from "elysia";
 import * as R from "remeda";
 import { startParsingAndDownload } from "./crons/downloader";
+import { getDesktopLinks } from "./crons/parser";
 import { db } from "./db";
 import { wallpapers } from "./db/schema";
-import { getDesktopLinks } from "./crons/parser";
 
 const app = new Elysia()
-  .use(swagger())
-  .use(
-    cron({
-      name: "heartbeat",
-      pattern: Patterns.everyHours(),
-      run() {
-        startParsingAndDownload();
-      },
-    })
-  )
-  .group("/api", (app) =>
-    app.get("/", async () => {
-      const desktopLinks = await getDesktopLinks();
+	.use(swagger())
+	.use(
+		cron({
+			name: "heartbeat",
+			pattern: Patterns.everyHours(),
+			run() {
+				startParsingAndDownload();
+			},
+		}),
+	)
+	.group("/api", (app) =>
+		app
+			.get("/", async () => {
+				const desktopLinks = await getDesktopLinks();
 
-      // Check for existing links
-      const query = await db
-        .select()
-        .from(wallpapers)
-        .where(inArray(wallpapers.originalUrl, desktopLinks));
+				// Check for existing links
+				const query = await db
+					.select()
+					.from(wallpapers)
+					.where(inArray(wallpapers.originalUrl, desktopLinks));
 
-      const diff = R.difference(
-        desktopLinks,
-        query.map((q) => q.originalUrl)
-      );
-      return diff;
-    })
-  )
-  .listen(process.env.PORT ?? 3000);
+				const diff = R.difference(
+					desktopLinks,
+					query.map((q) => q.originalUrl),
+				);
+				return diff;
+			})
+			.get("/all", async () => {
+				const wallpapersData = await db.select().from(wallpapers);
+				return wallpapersData;
+			}),
+	)
+	.listen(process.env.PORT ?? 3000);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+	`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
 );
